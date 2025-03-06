d99d9aaeb321f824291e02d4c3a4e8b6b4ddd49c
Yayun Liang <502024220065@smail.nju.edu.cn>
2025-03-06 10:52:27 +0800

    add compet_stimuli split_datasets1 is the updated code split_datasets is the original code

--- matlab-preprocessing-code/utils/extract_trials.m
+++ matlab-preprocessing-code/utils/extract_trials.m
@@ -5,11 +5,15 @@ trial_infos = struct("exg",[],"stimuli_path",[],"label",[],"env_path",[],"mel_pa
 for trial_idx = trial_idxs
     exg = NaN;
     stimuli_path = "";
+    compet_stimuli_path = "";
     label = "";
     env_path = "";
+    compet_env_path = "";
     mel_path = "";
     stimuli = [];
+    compet_stimuli = [];
     env = [];
+    compet_env = [];
     mel = [];
     stimuli_fs = [];
     switch dataset_name
@@ -18,9 +22,10 @@ for trial_idx = trial_idxs
                 exg = data_struct.data.eeg{trial_idx};
                 [~,label] = get_attention_directions(data_struct.expinfo,trial_idx);
                 stimuli_path = table2array(data_struct.expinfo(trial_idx,2+(data_struct.expinfo.attended_lr(trial_idx)=="right")));
+                compet_stimuli_path = table2array(data_struct.expinfo(trial_idx,2+(data_struct.expinfo.attended_lr(trial_idx)~="right")));
             end
-        case "Alices"
-            if trial_idx <= length(data_struct.data.eeg)
+        case {"Alices_raw"}
+            if trial_idx <= length(data_struct.data.exg)
                 exg = data_struct.data.exg{trial_idx};
                 stimuli_path = sprintf("DownTheRabbitHoleFinal_SoundFile%d.wav",trial_idx);
             end
@@ -34,38 +39,36 @@ for trial_idx = trial_idxs
                 exg = data_struct.(field){trial_idx}.RawData.EegData;
                 label = string(data_struct.(field){trial_idx}.attended_ear);
                 stimuli_path = string(data_struct.(field){trial_idx}.stimuli{1+(label=="R")});
+                compet_stimuli_path = string(data_struct.(field){trial_idx}.stimuli{1+(label~="R")});
                 split_stimuli = split(stimuli_path,"_");
+                compet_split_stimuli = split(compet_stimuli_path,"_");
                 env_path = sprintf("powerlaw subbands %s_dry.mat",join(split_stimuli(1:end-1),"_"));
+                compet_env_path = sprintf("powerlaw subbands %s_dry.mat",join(compet_split_stimuli(1:end-1),"_"));
             end
         case {"sparKULee_raw","sparKULee_preprocessed"}
             filelists = dir(fullfile(dataset_path,"sub-*","*","*.npy"));
             if trial_idx <= length(filelists)
-                exg = py.numpy.load(fullfile(filelists(trial_idx).folder,filelists(trial_idx).name))';
+                exg_py = py.numpy.load(fullfile(filelists(trial_idx).folder,filelists(trial_idx).name))';
+                exg = double(exg_py);
             end
-            target_audio = regexp(filelists(trial_idx).name,'.*?desc-preproc-audio-(.*?_\d_\d)_eeg.npy',"tokens");
+            target_audio = regexp(filelists(trial_idx).name, 'desc-preproc-audio-(audiobook(?:_\d+)+)_eeg\.npy', "tokens");
             env_path = target_audio{1}+"_-_envelope.npy";
             mel_path = target_audio{1}+"_-_mel.npy";
         case "DTU_preprocessed"
             if trial_idx <= length(data_struct.data.eeg)
                 exg = data_struct.data.eeg{trial_idx};
                 if int32(data_struct.data.event.eeg(trial_idx).value{:})==1
-                    label = 'left';
+                    label = "left";
                 else
-                    label = 'right';
+                    label = "right";
                 end
-                env = fastif(label==1,data_struct.data.wavA{trial_idx},data_struct.data.wavB{trial_idx});
-            end
-        case "MAD_EEG_preprocessed"
-            if trial_idx <= length(data_struct.data.eeg)
-                exg = data_struct.data.eeg{trial_idx};
-                label = int32(data_struct.data.event.eeg(trial_idx).value{:});
-                env = fastif(label==1,data_struct.data.wavA{trial_idx},data_struct.data.wavB{trial_idx});
+                env = fastif(label=="left",data_struct.data.wavA{trial_idx},data_struct.data.wavB{trial_idx});
+                compet_env = fastif(label=="right",data_struct.data.wavA{trial_idx},data_struct.data.wavB{trial_idx});
             end
         case "PKU-4talker-EEG_preprocessed"
             data_struct.EEG_space.data=data_struct.EEG_space.data';
                 exg = data_struct.EEG_space.data;
                 label = int32(data_struct.EEG_space.event.latency);
-                %env = fastif(label==1,data_struct.data.wavA{trial_idx},data_struct.data.wavB{trial_idx});
         case "Estart-2019_raw"
                 exg = data_struct.group__fM.("part_"+num2str(trial_idx))(:);
                 stimuli_path = "part_" + string(trial_idx) + "_story.wav";
@@ -76,6 +79,7 @@ for trial_idx = trial_idxs
                 exg = data_struct.data{1,trial_idx};
                 label =  string(data_struct.initAttention(1,trial_idx));
                 env = fastif(label=="left",data_struct.stimulus.leftEnvelopes{1,trial_idx},data_struct.stimulus.rightEnvelopes{1,trial_idx});
+                compet_env = fastif(label=="right",data_struct.stimulus.leftEnvelopes{1,trial_idx},data_struct.stimulus.rightEnvelopes{1,trial_idx});
         case "ASA_preprocessed"
                 exg = data_struct.data{1,trial_idx};
                 if data_struct.labels(trial_idx, 1) == 0
@@ -88,14 +92,18 @@ for trial_idx = trial_idxs
     end
     trial_infos(end).exg = exg;
     trial_infos(end).stimuli_path = stimuli_path;
+    trial_infos(end).compet_stimuli_path = compet_stimuli_path;
     trial_infos(end).label = string(label);
     trial_infos(end).env_path = env_path;
+    trial_infos(end).compet_env_path = compet_env_path;
     trial_infos(end).mel_path = mel_path;
     trial_infos(end).stimuli = stimuli;
+    trial_infos(end).compet_stimuli = compet_stimuli;
     trial_infos(end).env = env;
+    trial_infos(end).compet_env = compet_env;
     trial_infos(end).mel = mel;
     trial_infos(end).stimuli_fs = stimuli_fs;
-    trial_infos(end+1) = struct("exg",[],"stimuli_path",[],"label",[],"env_path",[],"mel_path",[],"stimuli",[],"env",[],"mel",[],"stimuli_fs",[]);
+    trial_infos(end+1) = struct("exg",[],"stimuli_path",[],"compet_stimuli_path",[],"label",[],"env_path",[],"compet_env_path",[],"mel_path",[],"stimuli",[],"compet_stimuli",[],"env",[],"compet_env",[],"mel",[],"stimuli_fs",[]);
 end
 end
 
--- matlab-preprocessing-code/utils/get_dataset_info.m
+++ matlab-preprocessing-code/utils/get_dataset_info.m
@@ -11,7 +11,7 @@ dataset_infos = struct("filelists",[], ...
 for dataset_name = dataset_names
     type = split(dataset_name,"_");
     type = type(2);
-    n_subject = [];
+    num_subject = [];
     disp(['Processing dataset: ', dataset_name]);  % 打印当前处理的 dataset_name
     switch dataset_name
         case {"NJU_preprocessed"}
@@ -27,15 +27,6 @@ for dataset_name = dataset_names
             channel = ["Cz"; "Fz"; "Fp1"; "F7"; "F3"; "FC1"; "C3"; "FC5"; "FT9"; "T7"; "CP5";
                 "CP1"; "P3"; "P7"; "PO9"; "O1"; "Pz"; "Oz"; "O2"; "PO10"; "P8"; "P4"; "CP2";
                 "CP6"; "T8"; "FT10"; "FC6"; "C4"; "FC2"; "F4"; "F8"; "Fp2"];
-        case "NJU_preproc"
-            base_path = "E:\EEG_RAW_DATA\NJU-15class-Emotiv-AAD\exg\preprocessed";
-            audio_path = "E:\EEG_RAW_DATA\NJU-15class-Emotiv-AAD\stimuli\";
-            fs = 128;
-            filelists = dir(fullfile(base_path,"S*.mat"));
-            nch = 32;
-            f_upper = 32;
-            num_trial = 32;
-            desired_length = 115 * fs;
         case {"KUL_raw","KUL_preprocessed"}
             base_path = "E:\EEG dataset\KUL";
             audio_path = "E:\EEG dataset\KUL\stimuli\stimuli";
@@ -45,18 +36,20 @@ for dataset_name = dataset_names
             f_upper = 64;
             num_trial = 20;
             desired_length = 1.2e4;
+            channel = [];
         case "Alices_raw"
-            base_path = "E:\EEG_RAW_DATA\BrennansAliceStory\aligned_eeg";
-            audio_path = "E:\EEG_RAW_DATA\BrennansAliceStory\audio\audio";
+            base_path = "E:\EEG_dataset_Superhuge\BrennansAliceStory\aligned_eeg";
+            audio_path = "E:\EEG_dataset_Superhuge\BrennansAliceStory\audio\audio";
             fs = 500;
             filelists = dir(fullfile(base_path,"S*.mat"));
             nch = 60;
             f_upper = 200;
             num_trial = 12;
             desired_length = 28e3;
+            channel = [];
         case {"sparKULee_raw","sparKULee_preprocessed"}
-            base_path = sprintf("E:\\EEG_RAW_DATA\\sparrKULee\\derivatives\\%s_eeg",type);
-            audio_path = "E:\EEG_RAW_DATA\sparrKULee\derivatives\preprocessed_stimuli";
+            base_path = sprintf("E:\\EEG_dataset_Superhuge\\sparrKULee\\sparrKULee\\derivatives\\%s_eeg",type);
+            audio_path = "E:\EEG_dataset_Superhuge\sparrKULee\sparrKULee\derivatives\preprocessed_stimuli";
             fs = 128;
             tmp_list = dir(fullfile(base_path,"sub-*","ses*"));
             f = fieldnames(tmp_list)';
@@ -71,7 +64,8 @@ for dataset_name = dataset_names
             f_upper = 64;
             num_trial = 10;
             desired_length = 10e4;
-            n_subject = length(dir(fullfile(base_path,"sub-*")));
+            num_subject = length(dir(fullfile(base_path,"sub-*")));
+            channel = [];
         case "DTU_preprocessed"
             base_path = "E:\EEG dataset\DTU\DATA_preproc";
             audio_path = "";
@@ -88,17 +82,6 @@ for dataset_name = dataset_names
                 "FCz"; "Cz"; "C2"; "C4"; "C6"; "T8"; "TP8"; "CP6"; "CP4"; "CP2"; "P2"; "P4"; 
                 "P6"; "P8"; "P10"; "PO8"; "PO4"; "O2"; "EXG1"; "EXG2";
 ];
-        case "MAD-EEG_preprocessed"
-            base_path = "E:\\EEG dataset\\MAD-EEG\\madeeg_preprocessed.hdf5"
-            audio_path = "E:\\EEG dataset\\MAD-EEG\\stimuli\\stimuli";
-            fs = 256;
-            filelists = [1];
-            nch = 20;
-            f_upper = 128;
-            desired_length = 5052;
-            num_trial = 22;
-            num_subject = 16;
-            dataset_infos.filelists = filelists;
         case "PKU-4talker-EEG_preprocessed"
             base_path = "E:\EEG dataset\PKU-4talker-EEG\preprocess_data\data_space";
             audio_path = "";
@@ -109,7 +92,6 @@ for dataset_name = dataset_names
             num_trial = 40;
             num_subject = 16;
             desired_length = 7680;
-            dataset_infos.filelists = filelists;
             channel = [
                 "Fpz"; "Fp1"; "Fp2"; "AF3"; "AF4"; "AF7"; "AF8"; "Fz"; "F1"; "F2"; 
                 "F3"; "F4"; "F5"; "F6"; "F7"; "F8"; "FCz"; "FC1"; "FC2"; "FC3"; 
@@ -120,7 +102,7 @@ for dataset_name = dataset_names
                 "HEOR"; "HEOL"; "VEOU"; "VEOL"
             ];
         case "Estart-2019_raw"
-            base_path = "E:\EEG_dataset_Superhuge\Estart_2019\eeg"
+            base_path = "E:\EEG_dataset_Superhuge\Estart_2019\eeg";
             audio_path = "E:\EEG_dataset_Superhuge\Estart_2019\audiobooks\audiobooks\fM";
             fs = 1000;
             filelists = dir(fullfile(base_path,"*.h5"));
@@ -129,7 +111,6 @@ for dataset_name = dataset_names
             desired_length = 140000;
             num_trial = 4;
             num_subject = 18;
-            dataset_infos.filelists = filelists;
             channel = [];
         case "Data-for-CS_preprocessed"
             base_path = "E:\EEG dataset\Data_for_CS 安徽大学\Data_for_CS\data_for_CS\audio_label";
@@ -139,7 +120,7 @@ for dataset_name = dataset_names
             nch = 32;
             f_upper = 64;
             num_trial = 4352;
-            number_subject = 8;
+            num_subject = 8;
             desired_length = 128;
         case "KUL-AV-GC_preprocessed"
             base_path = "E:\EEG dataset\KUL_AV_GC";
@@ -185,7 +166,7 @@ for dataset_name = dataset_names
     dataset_infos(end).fs = fs;
     dataset_infos(end).nch = nch;
     dataset_infos(end).num_trial = num_trial;
-    dataset_infos(end).num_subject = fastif(isempty(n_subject),nfile,n_subject);
+    dataset_infos(end).num_subject = fastif(isempty(num_subject),nfile,num_subject);
     dataset_infos(end).audio_path = audio_path;
     dataset_infos(end).base_path = base_path;
     dataset_infos(end).channel = channel;  % 新增 channel 字段

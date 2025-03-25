23abb7ec944737c7424ae51c6731dd28753d2792
YuanmingZhang <641994329@qq.com>
2025-03-25 14:09:47 +0800

    testing original pytorch models

--- superhuge/utils/transforms/pad_speech.py
+++ superhuge/utils/transforms/pad_speech.py
@@ -9,7 +9,7 @@ class PadSpeech(Transform):
 
     def __call__(self, data: torch.Tensor):
         if data.ndim == 1:
-            data = np.stack([data, np.zeros_like(data)], axis=1)
-        elif data.ndim == 2 and data.shape[1] == 1:
-            data = np.concatenate([data, np.zeros_like(data)], axis=1)
+            data = np.stack([data, np.zeros_like(data)], axis=-1)
+        elif data.shape[-1] == 1:
+            data = np.concatenate([data, np.zeros_like(data)], axis=-1)
         return data

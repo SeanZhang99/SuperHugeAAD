7ad29049f3ae5ea40e1d02054815b7ba4246f00f
YuanmingZhang <641994329@qq.com>
2026-07-10 11:26:07 +0800

    update the resample logics

--- superhuge/data/transforms/resample.py
+++ superhuge/data/transforms/resample.py
@@ -1,7 +1,7 @@
-from typing import Any
+from typing import Any, Literal
 from warnings import warn
 import numpy as np
-from scipy.signal import resample
+from scipy.signal import resample, decimate, resample_poly
 
 from .abc import Transform
 
@@ -10,7 +10,13 @@ class Resample(Transform):
     """Reduces EEG sampling rate to improve efficiency."""
 
     def __init__(
-        self, /, *, old_fs: int | float, new_fs: int | float, **kwargs
+        self,
+        /,
+        *,
+        old_fs: int | float,
+        new_fs: int | float,
+        method: Literal["resample", "decimate", "resample_poly"] = "resample_poly",
+        **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         if kwargs["when"] == "before_returning":
@@ -19,8 +25,22 @@ class Resample(Transform):
             )
         self.old_fs = old_fs
         self.new_fs = new_fs
+        self.method = method
 
     def __call__(self, x: np.ndarray, **kwargs) -> dict[str, np.ndarray]:
         super().__call__(x)
-        num_samples = int(x.shape[0] * self.new_fs / self.old_fs)
-        return {"x": resample(x, num_samples, axis=0)}  # type: ignore
+        if self.method == "resample":
+            num_samples = int(x.shape[0] * self.new_fs / self.old_fs)
+            return {"x": resample(x, num_samples, axis=0)}  # type: ignore
+        elif self.method == "decimate":
+            decimation_factor = int(self.old_fs / self.new_fs)
+            assert (
+                self.old_fs % self.new_fs == 0
+            ), "Old sampling rate must be an integer multiple of new sampling rate for decimation."
+            return {"x": decimate(x, decimation_factor, axis=0)}  # type: ignore
+        elif self.method == "resample_poly":
+            return {"x": resample_poly(x, self.new_fs, self.old_fs, axis=0)}  # type: ignore
+        else:
+            raise ValueError(
+                f"Invalid resampling method: {self.method}. Choose from 'resample', 'decimate', or 'resample_poly'."
+            )
--- superhuge/data/transforms/zscore.py
+++ superhuge/data/transforms/zscore.py
@@ -16,7 +16,7 @@ class ZScore(Transform):
 
     def __call__(self, x: np.ndarray, **kwargs) -> dict[str, np.ndarray]:
         super().__call__(x, **kwargs)
-        eps = kwargs.get("eps", 1.0e-5)
+        eps = kwargs.get("eps", 1.0e-8)
         x -= x.mean(axis=0, keepdims=True)
         x /= x.std(axis=0, keepdims=True) + eps
         return {"x": x}
